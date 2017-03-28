c64860e2ff978f1dfb6e2f69baf8cc1e64ba6a90
mikelkra <mikelkrasniqi@gmail.com>
2017-03-28 17:11:41 +0200

    Update Conn.Class.php

--- controller/private/Conn.Class.php
+++ controller/private/Conn.Class.php
@@ -1,28 +1,16 @@
 <?php
-// $db = new Conn();
-
-// $getRow = $db->getRow("SELECT * FROM users WHERE username = ?", ["admin"]);
-// $getRows = $db->getRows("SELECT * FROM users");
-// $insertRow = $db->insertRow("INSERT INTO users(username, password, email) VALUE(?, ?, ?)", ["Arthur", "102030", "arthur@localhost"]);
-// $updateRow = $db->updateRow("UPDATE users SET username = ?, password = ? WHERE id = ?", ["ArthurMann", "a123456", "7"]);
-// $deleteRow = $db->deleteRow("DELETE FROM users WHERE id = ?", [8]);
-
-
-// $db->Disconnect();
-
-
 class Conn {
 	public $isConn;
 	protected $datab;
-	private $username = "root";
-	private $password = "";
-	private $host = "localhost";
-	private $dbname = "loginsystem";
+	private $username = "username";
+	private $password = "password";
+	private $host = "hostname";
+	private $dbname = "database name";
 	private $options = [];
 	
 	// connect to db
 	public function __construct() {
-		$this->isConn = TRUE;
+		$this->connected = TRUE;
 		try {
 			$this->datab = new PDO ( "mysql:host={$this->host};dbname={$this->dbname};charset=utf8", $this->username, $this->password, $this->options );
 			$this->datab->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -35,50 +23,43 @@ class Conn {
 	// disconnect from db
 	public function Disconnect() {
 		$this->datab = NULL;
-		$this->isConn = FALSE;
+		$this->connected = FALSE;
 	}
 	
-	//get last id
+	//get last id (only for incremented id)
 	public function getLastId(){
 		$id = $this->datab->lastInsertId();
 		return $id;
 	}
-	// get row
-	public function getRow($query, $params = []) {
+	// get one row
+	public function getRow($query, $values = []) {
 		try {
 			$stmt = $this->datab->prepare ( $query );
-			$stmt->execute ( $params );
+			$stmt->execute ( $values );
 			return $stmt->fetch ();
 		} catch ( PDOException $e ) {
 			throw new Exception ( $e->getMessage () );
 		}
 	}
 	// get rows
-	public function getRows($query, $params = []) {
+	public function getRows($query, $values = []) {
 		try {
 			$stmt = $this->datab->prepare ( $query );
-			$stmt->execute ( $params );
+			$stmt->execute ( $values );
 			return $stmt->fetchAll ();
 		} catch ( PDOException $e ) {
 			throw new Exception ( $e->getMessage () );
 		}
 	}
 	// insert row
-	public function insertRow($query, $params = []) {
+	public function insertRow($query, $values = []) {
 		try {
 			$stmt = $this->datab->prepare ( $query );
-			$stmt->execute ( $params );
+			$stmt->execute ( $values );
 			return TRUE;
 		} catch ( PDOException $e ) {
 			throw new Exception ( $e->getMessage () );
 		}
 	}
-	// update row
-	public function updateRow($query, $params = []) {
-		$this->insertRow ( $query, $params );
-	}
-	// delete row
-	public function deleteRow($query, $params = []) {
-		$this->insertRow ( $query, $params );
-	}
+
 }
